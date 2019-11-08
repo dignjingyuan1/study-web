0815ead8537c5c38f18d972c91f0b1524a1c9d6a
fanzhiyu1 <>
2019-11-08 14:14:51 +0800

    用户登录区分pc和移动

--- template/js/question.js
+++ template/js/question.js
@@ -2,7 +2,7 @@ define([], function () {
 	
 	// controller
 	return ["$scope","$state", function ($scope,$state) {
-		
+		var timeOut;
 		Pager.index = 1;
 		Pager.limit = 6;
 		$scope.searchProblemList = function(){
@@ -14,7 +14,6 @@ define([], function () {
 					pageSize: Pager.limit
 				},
 				callback: function(res){
-					console.log(res);
 					if(res.code == '2000'){
 						$scope.problemList = res.rows;
 						Pager.total = res.total;
@@ -37,7 +36,84 @@ define([], function () {
 		 * 问题详细
 		 */
 		$scope.questionDetails = function(id){
-			$state.go("question-details",{id:id})
+			if(isUserLogin()){
+				_post({
+					url: STUDY_API + "/problemFollow/createProblemFollow",
+					param: {
+						problemId: id,
+						client: '1'
+					},
+					callback: function(res){
+						console.log(res);
+						if(res.code == '2000'){
+							var data = res.data;
+							if(data){
+								showPopup({
+									url : 'select-wxpay',
+									title: '微信支付',
+									callback: function(){
+										$("#zfbcode").qrcode(data.qrcode);
+									}
+								});
+								createTime = new Date();
+								$scope.searchOrderStatus(data.orderId);
+							}else{
+								$state.go("question-details",{id:id});
+							}
+						}
+					}
+				})
+				return ;
+			}
+			$("#login-alert").show();
+			setTimeout(()=>{
+				$("#login-box").css("transform","scale(1)");
+			},50);
+			
+		}
+		
+		/**
+		 * 查找订单详细
+		 * @param {Object} orderId
+		 */
+		$scope.searchOrderStatus = function(orderId){
+			_get({
+				url: STUDY_API + "/order/getOrderDetails",
+				param: {
+					orderId: orderId
+				},
+				loading: false,
+				callback: function(res){
+					var data = res.data;
+					if(data.orderStatus == "SUCCESS"){
+						$(".pay-body").empty();
+						var now = new Date();
+						var html = "<div class='pay-success'>" +
+									"<i class='icon iconfont'></i>" +
+									"<span class='success-title'>您已支付成功！请到我的订单中查看。</span>" +
+									"</div>" +
+									"<div class='clear'>" +
+									"<div class='pay-close'>"+
+									"<span class='success-date'>支付时间：" + now.Format("yyyy-MM-dd hh:mm:ss") + "</span>" +
+									"</div>"+
+									"<div class='pay-close'>" +
+									"</div>";
+						$(".pay-body").append(html);
+						clearTimeout(timeOut);
+						return;
+					}
+					var now = new Date();
+					if((createTime.getTime()+5*60*1000) < now.getTime()){
+						clearTimeout(timeOut);
+						closePopup();
+						return;
+					}
+					timeOut = setTimeout(function(){
+						$scope.searchOrderStatus(orderId);
+					}, 1000);
+				}
+			});
+			
 		}
 		
 		/**
