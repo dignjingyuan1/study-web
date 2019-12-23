5cc8ee2557edcffad5b0f308ff6e49d891bfcd20
DJY <534215871@qq.com>
2019-12-23 23:09:04 +0800

    添加很多东西

--- template/phone-js/mine-course.js
+++ template/phone-js/mine-course.js
@@ -4,5 +4,33 @@ define([], function () {
 		$scope.go = function(text){
 			$state.go(text)
 		}
+		var user = localStorage.getItem('user');
+		$scope.userInfo = null;
+		if(user){
+			$scope.userInfo = JSON.parse(user);
+			if(!$scope.userInfo.userHeader){
+				$scope.userInfo.userHeader = "img/defalte-head.jpg"
+			}
+		}
+		console.log("userInfo:", $scope.userInfo)
+
+		/**
+		 * 查找订单列表
+		 */
+		$scope.searchOrderList = function(){
+			_get({
+				url: STUDY_API + "/order/getOrderList",
+				param: {
+					pageNo: 1,
+					pageSize: 100,
+				},
+				callback: function(res){
+					if(res.code == '2000'){
+						$scope.orderList = res.rows;
+						$scope.$applyAsync();
+					}
+				}
+			})
+		}
 	}];
 });
--- template/phone-js/mine.js
+++ template/phone-js/mine.js
@@ -4,5 +4,20 @@ define([], function () {
 		$scope.go = function(text){
 			$state.go(text)
 		}
+		var user = getUser();
+		$scope.userInfo = null;
+		if(user){
+		    $scope.userInfo = user;
+		    if(!$scope.userInfo.userHeader){
+		        $scope.userInfo.userHeader = "img/defalte-head.jpg"
+		    }
+		}
+		console.log("userInfo:", $scope.userInfo)
+		$scope.logOut = function () {
+			_confirm("确定要退出么？",function(){
+				localStorage.clear();
+				$state.go("phone-home");
+			});
+		}
 	}];
 });
