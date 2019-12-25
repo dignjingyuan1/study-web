bc22f98cc03a9a566c120d5debb098b00466d862
jingyuan.ding <534215871@qq.com>
2019-12-25 16:09:42 +0800

    判断登录

--- template/phone-js/mine.js
+++ template/phone-js/mine.js
@@ -1,23 +1,28 @@
 define([], function () {
-	// controller
-	return ["$scope","$state", function ($scope,$state) {
-		$scope.go = function(text){
-			$state.go(text)
-		}
-		var user = getUser();
-		$scope.userInfo = null;
-		if(user){
-		    $scope.userInfo = user;
-		    if(!$scope.userInfo.userHeader){
-		        $scope.userInfo.userHeader = "img/defalte-head.jpg"
-		    }
-		}
-		console.log("userInfo:", $scope.userInfo)
-		$scope.logOut = function () {
-			_confirm("确定要退出么？",function(){
-				localStorage.clear();
-				$state.go("phone-home");
-			});
-		}
-	}];
+    // controller
+    return ["$scope", "$state", function ($scope, $state) {
+        $scope.go = function (text) {
+            $state.go(text)
+        }
+        $scope.goto = function (text) {
+            if ($scope.userInfo) {
+                $state.go(text)
+            } else {
+				$state.go("phone-login");
+            }
+        }
+        var user = getUser();
+        $scope.userInfo = null;
+        if (user) {
+            $scope.userInfo = user;
+            if (!$scope.userInfo.userHeader) {
+                $scope.userInfo.userHeader = "img/defalte-head.jpg"
+            }
+        }
+        console.log("userInfo:", $scope.userInfo)
+        $scope.logOut = function () {
+			localStorage.clear();
+			$state.go("phone-home");
+        }
+    }];
 });
