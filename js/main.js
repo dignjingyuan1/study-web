01bd1b8a70019a8291ae50c90632eaa7faa7594a
dingjingyuan <534215871@qq.com>
2020-03-08 21:00:17 +0800

    添加隐私政策和用户须知

--- js/main.js
+++ js/main.js
@@ -318,7 +318,7 @@ function closeWebsocket() {
  */
 //function loginWebsocket(stomp){
 //	var _token = sessionStorage.getItem("token");
-//	var _refreshToken = sessionStorage.getItem("refreshToken");	
+//	var _refreshToken = sessionStorage.getItem("refreshToken");
 //	$.ajax({
 //		url:"http://localhost:8088/login",
 //		type:"post",
@@ -452,4 +452,48 @@ function isUserLogin() {
         }
     });
     return flag;
+}
+
+/**
+ * 微信支付
+ */
+function wxPay(orderId, callback){
+    wx.config({
+        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        appId: '', // 必填，公众号的唯一标识
+        timestamp: '', // 必填，生成签名的时间戳
+        nonceStr: '', // 必填，生成签名的随机串
+        signature: '',// 必填，签名
+        jsApiList: [] // 必填，需要使用的JS接口列表
+    });
+    function onBridgeReady(){
+        WeixinJSBridge.invoke(
+            'getBrandWCPayRequest', {
+                "appId":"wx2421b1c4370ec43b",     //公众号名称，由商户传入 固定的
+                "timeStamp":"1395712654",         //时间戳，自1970年以来的秒数 计算出来
+                "nonceStr":"e61463f8efa94090b1f366cccfbbb444", //随机串 生成的
+                "package":"prepay_id=u802345jgfjsdfgsdg888", // 订单号
+                "signType":"MD5",         //微信签名方式：
+                "paySign":"70EA570631E4BB79628FBCA90534C63FF7FADD89" //微信签名 应该是固定的
+            },
+            function(res){
+                if(res.err_msg == "get_brand_wcpay_request:ok" ){
+                    // 使用以上方式判断前端返回,微信团队郑重提示：
+                    //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
+                    callback(true)
+                }else {
+                    callback(false)
+                }
+            });
+    }
+    if (typeof WeixinJSBridge == "undefined"){
+        if( document.addEventListener ){
+            document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+        }else if (document.attachEvent){
+            document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+            document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+        }
+    }else{
+        onBridgeReady();
+    }
 }
